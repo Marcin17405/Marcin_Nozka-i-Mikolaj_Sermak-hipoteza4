bcf25478135370b7dfd1892905bb79de6404f2f7
student <student@ckziu.jaworzno.pl>
2022-12-09 14:00:09 +0100

    in work

--- venv/cezar.py
+++ venv/cezar.py
@@ -1,13 +1,23 @@
-niezaszyfrowany = "cezar"
-
-def szyfr(niezaszyfrowany):
-    zaszyfrowany = ""
-    for i in range(len(niezaszyfrowany)):
-        klucz = i+1
-        if ord(niezaszyfrowany[i]) > 122 - klucz:
-            zaszyfrowany += chr(ord(niezaszyfrowany[i]) + klucz - 26)
+def encrypt(decrypted):
+    encrypted = ""
+    for i in range(len(decrypted)):
+        key = i + 1
+        if ord(decrypted[i]) > 122 - key:
+            encrypted += chr(ord(decrypted[i]) + key - 26)
         else:
-            zaszyfrowany += chr(ord(niezaszyfrowany[i]) + klucz)
-    print("Zaszyfrowany tekst:", zaszyfrowany)
+            encrypted += chr(ord(decrypted[i]) + key)
+    return encrypted
 
-print(szyfr(niezaszyfrowany))
+def decrypt(encrypted):
+    decrypted = ""
+    for i in encrypted:
+        key = i + 1
+        if (ord(encryped) - key < 97):
+            decrypted += chr(ord(encrypted) - key + 26)
+        else:
+            decrypted += chr(ord(encrypted) - key)
+    return decrypted
+
+
+# print(encrypt("abc"))
+print(decrypt("bdf"))

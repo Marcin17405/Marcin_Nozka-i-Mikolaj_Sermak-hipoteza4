e96b3ec64f8866662f44deed63801421db3202d7
student <student@ckziu.jaworzno.pl>
2022-12-09 10:47:17 +0100

    funkcja

--- venv/cezar.py
+++ venv/cezar.py
@@ -1,11 +1,13 @@
 niezaszyfrowany = "cezar"
-zaszyfrowany = ""
 
-for i in range(len(niezaszyfrowany)):
-    klucz = i+1
-    if ord(niezaszyfrowany[i]) > 122 - klucz:
-        zaszyfrowany += chr(ord(niezaszyfrowany[i]) + klucz - 26)
-    else:
-        zaszyfrowany += chr(ord(niezaszyfrowany[i]) + klucz)
+def szyfr(niezaszyfrowany):
+    zaszyfrowany = ""
+    for i in range(len(niezaszyfrowany)):
+        klucz = i+1
+        if ord(niezaszyfrowany[i]) > 122 - klucz:
+            zaszyfrowany += chr(ord(niezaszyfrowany[i]) + klucz - 26)
+        else:
+            zaszyfrowany += chr(ord(niezaszyfrowany[i]) + klucz)
+    print("Zaszyfrowany tekst:", zaszyfrowany)
 
-print("Zaszyfrowany tekst:", zaszyfrowany)
+print(szyfr(niezaszyfrowany))
